7357b92b86f7295ade621199955e4bd76d517d1f
Muhammad Umair Akram <165748503+Umair-Alvi@users.noreply.github.com>
2024-04-19 08:59:00 -0700

    Update page.js
    click

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
       <>
+      download the Load sheet click hear:
          <h1><Link href="/loadSheet"> Load Sheet </Link></h1>
         
       </>
